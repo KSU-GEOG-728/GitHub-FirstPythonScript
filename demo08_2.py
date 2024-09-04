e4a7de709334ba2328660cedecad5734018715d6
Shawn Hutchinson <shutch@ksu.edu>
2024-09-04 07:53:42 -0500

    Update demo08_2.py

--- demo08_2.py
+++ demo08_2.py
@@ -5,14 +5,14 @@
 	File name: demo08_2.py
 	Author:  Shawn Hutchinson
 	Description: Example script that selects Kansas counties through which a major river passes
-	Data created: 9/13/2023
+	Data created: 9/4/2024
 	Python version: 3.9.16
 """
 
 # Import arcpy module and set environments
 import arcpy
 arcpy.env.overwriteOutput = True
-arcpy.env.workspace = "D:/GIS_Teaching/GEOG728_TeachingProjects/Exercise04/Exercise04.gdb"
+arcpy.env.workspace = "D:/GIS_Teaching/GEOG728_TeachingProjects/GitHub-FirstPythonScript/ExerciseData.gdb"
 
 # Establish local variables
 inputFeatures = "ks_major_rivers"
@@ -22,7 +22,5 @@ outputFeatures = "SelectedCounties"
 
 # Perform geoprocessing
 selectRiver = arcpy.management.SelectLayerByAttribute(inputFeatures, "NEW_SELECTION", sqlExpression)
-
 selectCounties = arcpy.management.SelectLayerByLocation(selectFeatures, "INTERSECT", selectRiver, "", "NEW_SELECTION")
-
 arcpy.management.CopyFeatures(selectCounties, outputFeatures)
